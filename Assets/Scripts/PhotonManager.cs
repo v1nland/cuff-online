9ce624c35f1ffb8e508271a662d24ca3c4ab8925
v1nland <martin.saavedra@mail.udp.cl>
2020-05-05 00:50:16 -0400

    First commit. (#1)

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster() {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby() {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions {  MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom() {
+        PhotonNetwork.Instantiate("Player", transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 7f;
+
+    float horizontalInput;
+    float verticalInput;
+
+    void Update () {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate () {
+        float horizontalSpeed = horizontalInput * Time.fixedDeltaTime * movementSpeed;
+        float verticalSpeed = verticalInput * Time.fixedDeltaTime * movementSpeed;
+
+        transform.Translate(new Vector3(horizontalSpeed, 0f, 0f));
+        transform.Translate(new Vector3(0f, verticalSpeed, 0f));
+    }
+}
